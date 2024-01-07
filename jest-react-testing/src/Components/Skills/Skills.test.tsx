9d2eacb3e6d9d175168415a249054474aabd3624
ShivamTaneja <shiivamtaneja@gmail.com>
2024-01-07 18:17:41 +0530

    add multiple query RTL test

---
+++ jest-react-testing/src/Components/Skills/Skills.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { Skills } from "./Skills";
+
+describe('Skills', () => {
+  const skills = ['HTML', 'CSS', 'Javascript'];
+
+  test('renders correctly', () => {
+    render(<Skills skills={skills} />);
+
+    const listElement = screen.getByRole('list');
+    expect(listElement).toBeInTheDocument();
+  });
+
+  test('renders list of skills', () => {
+    render(<Skills skills={skills} />);
+
+    const listItemElements = screen.getAllByRole('listitem');
+    expect(listItemElements).toHaveLength(skills.length);
+  });
+});
