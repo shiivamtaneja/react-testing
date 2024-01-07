0ded4f82887ef9f7d8c9ef27d12c934d057f14bf
ShivamTaneja <shiivamtaneja@gmail.com>
2024-01-07 15:22:03 +0530

    add test coverage using jest

---
+++ jest-react-testing/src/Components/Greet2/Greet.types.ts
@@ -0,0 +1,3 @@
+export type GreetWithNameProps = {
+  name?: string
+};
--- jest-react-testing/src/Components/Greet2/GreetWithName.tsx
+++ jest-react-testing/src/Components/Greet2/GreetWithName.tsx
@@ -1,6 +1,5 @@
-type GreetWithNameProps = {
-  name?: string
-}
+import { GreetWithNameProps } from "./Greet.types"
+
 export const GreetWithName = (props: GreetWithNameProps) => {
   return (
     <div>Hello {props.name}</div>
