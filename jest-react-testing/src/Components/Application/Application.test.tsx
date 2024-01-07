206e93c5705a47613aab1635ebdaa3d1a83e5f6e
ShivamTaneja <shiivamtaneja@gmail.com>
2024-01-07 17:27:43 +0530

    add options to getByRole

--- jest-react-testing/src/Components/Application/Application.test.tsx
+++ jest-react-testing/src/Components/Application/Application.test.tsx
@@ -4,9 +4,30 @@ import { Application } from "./Application";
 describe('Application', () => {
   test('renders corrrectly', () => {
     render(<Application />);
-    
-    const nameElement = screen.getByRole('textbox');
+
+    const pageHeading = screen.getByRole('heading', {
+      // Instead of using Name we can use level
+      // name: 'Job Application Form',
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole('heading', {
+      // Instead of using Name we can use level
+      // name: 'Section 1',
+      level: 2
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
+    const nameElement = screen.getByRole('textbox', {
+      name: 'Name'
+    });
     expect(nameElement).toBeInTheDocument();
+
+    const bioElement = screen.getByRole('textbox', {
+      name: 'Bio'
+    });
+    expect(bioElement).toBeInTheDocument();
 
     const jobLocationElement = screen.getByRole('combobox');
     expect(jobLocationElement).toBeInTheDocument();
